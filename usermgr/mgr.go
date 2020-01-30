e6399940948ccf982453c2c67da39ae7f215c6ee
zhangjin <zhangjin@cmcm.com>
2020-01-30 23:21:45 +0800

    perf:建表语句

--- usermgr/mgr.go
+++ usermgr/mgr.go
@@ -1,30 +1,39 @@
 package usermgr
 
 import (
+	"fmt"
+
 	uuidplus "github.com/cheetah-fun-gs/goplus/uuid"
 	"github.com/cheetah-fun-gs/gouser/authmgr"
 	"github.com/cheetah-fun-gs/gouser/tokenmgr"
 	redigo "github.com/gomodule/redigo/redis"
 )
 
+type modelTable struct {
+	Name      string
+	CreateSQL string
+}
+
 // UserMgr 用户管理器
 type UserMgr struct {
 	tokenmgr          tokenmgr.TokenMgr               // token 管理器
+	tableUser         *modelTable                     // 用户表
+	tableAuth         *modelTable                     // 第三方认证表
+	tableAccessKey    *modelTable                     // 访问密钥表
 	sendEmailCode     func(email, code string) error  // 发送邮箱验证码
 	sendMobileCode    func(mobile, code string) error // 发送短信验证码
 	generateUID       func() (uid, extra string)      // 生成一个全新的uid和扩展信息
 	generateAccessKey func(uid string) string         // 生成一个全新的AccessKey
-	authMgrs          []authmgr.AuthMgr               // 支持的认证方式
+	authMgrs          []authmgr.AuthMgr               // 支持的第三方认证方式
 	config            *Config
 	name              string
 }
 
 // Config ...
 type Config struct {
-	TokenExpire        int    // token 超时时间
-	IsOpenAccessKey    bool   // 是否打开访问密钥功能
-	UIDFieldType       string // uid 字段类型 默认 char(22)
-	AccessKeyFieldType string // access key 字段类型 默认 char(22)
+	TokenExpire        int  // token 超时时间
+	IsSupportAuth      bool // 是否支持第三方认证
+	IsSupportAccessKey bool // 是否支持访问密钥
 }
 
 func defaultGenerateUID() (uid, extra string) {
@@ -51,16 +60,26 @@ func New(name string, pool *redigo.Pool, args ...interface{}) *UserMgr {
 		config = args[0].(*Config)
 	} else {
 		config = &Config{
-			TokenExpire:        3600 * 2,
-			UIDFieldType:       "Char(22)",
-			AccessKeyFieldType: "Char(22)",
+			TokenExpire: 3600 * 2,
 		}
 	}
 
 	mgr := &UserMgr{
-		name:              name,
-		config:            config,
-		tokenmgr:          tokenmgr.New(name, pool, config.TokenExpire),
+		name:     name,
+		config:   config,
+		tokenmgr: tokenmgr.New(name, pool, config.TokenExpire),
+		tableUser: &modelTable{
+			Name:      name + "_user",
+			CreateSQL: fmt.Sprintf(TableUser, name+"_user"),
+		},
+		tableAuth: &modelTable{
+			Name:      name + "_user_auth",
+			CreateSQL: fmt.Sprintf(TableAuth, name+"_user_auth"),
+		},
+		tableAccessKey: &modelTable{
+			Name:      name + "_user_access_key",
+			CreateSQL: fmt.Sprintf(TableAccessKey, name+"_user_access_key"),
+		},
 		generateUID:       defaultGenerateUID,
 		generateAccessKey: defaultGenerateAccessKey,
 		sendEmailCode:     defaultSendEmailCode,
@@ -97,4 +116,28 @@ func (mgr *UserMgr) SetGenerateUID(v func() (uid, extra string)) {
 // SetGenerateAccessKey ...
 func (mgr *UserMgr) SetGenerateAccessKey(v func(uid string) string) {
 	mgr.generateAccessKey = v
+}
+
+// SetTableUser ...
+func (mgr *UserMgr) SetTableUser(tableName, tableCreateSQL string) {
+	mgr.tableUser = &modelTable{
+		Name:      tableName,
+		CreateSQL: tableCreateSQL,
+	}
+}
+
+// SetTableAuth ...
+func (mgr *UserMgr) SetTableAuth(tableName, tableCreateSQL string) {
+	mgr.tableAuth = &modelTable{
+		Name:      tableName,
+		CreateSQL: tableCreateSQL,
+	}
+}
+
+// SetTableAccessKey ...
+func (mgr *UserMgr) SetTableAccessKey(tableName, tableCreateSQL string) {
+	mgr.tableAccessKey = &modelTable{
+		Name:      tableName,
+		CreateSQL: tableCreateSQL,
+	}
 }
--- usermgr/model.go
+++ usermgr/model.go
@@ -1,18 +1,77 @@
 package usermgr
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
+
+// sql table
+const (
+	TableUser = `CREATE TABLE IF NOT EXISTS %v (
+		id int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
+		uid char(22) NOT NULL COMMENT '用户ID',
+		password char(22) NOT NULL COMMENT '密码',
+		email varchar(45) DEFAULT NULL COMMENT '邮箱',
+		mobile varchar(45) DEFAULT NULL COMMENT '手机号',
+		nickname varchar(64) NOT NULL COMMENT '昵称',
+		avatar varchar(1024) NOT NULL COMMENT '头像',
+		extra varchar(1024) NOT NULL COMMENT '扩展信息',
+		last_login datetime NOT NULL COMMENT '最后登录时间',
+		created datetime NOT NULL COMMENT '创建时间',
+		updated datetime NOT NULL COMMENT '更新时间',
+		PRIMARY KEY (id),
+		UNIQUE KEY uniq_uid (uid),
+		UNIQUE KEY uniq_email (email),
+		UNIQUE KEY uniq_mobile (mobile),
+		KEY idx_nickname (nickname),
+		KEY idx_last_login (last_login),
+		KEY idx_created (created),
+		KEY idx_updated (updated)
+	  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表'`
+	TableAuth = `CREATE TABLE IF NOT EXISTS %v (
+		id int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
+		uid char(22) NOT NULL COMMENT 'uid',
+		auth_name varchar(45) NOT NULL COMMENT '认证名称',
+		auth_uid varchar(128) NOT NULL COMMENT '第三方唯一ID',
+		auth_extra varchar(1024) NOT NULL COMMENT '第三方信息',
+		created datetime NOT NULL COMMENT '创建时间',
+		updated datetime NOT NULL COMMENT '更新时间',
+		PRIMARY KEY (id),
+		UNIQUE KEY uniq_auth_uid (auth_uid),
+		UNIQUE KEY uniq_uid_auth_name (uid,auth_name),
+		KEY idx_created (created),
+		KEY idx_updated (updated)
+	  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='第三方认证表'`
+	TableAccessKey = `CREATE TABLE IF NOT EXISTS %v (
+		id int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
+		access_key char(22) NOT NULL COMMENT '访问密钥',
+		uid char(22) NOT NULL COMMENT '用户ID',
+		expire_at datetime NOT NULL COMMENT '到期时间',
+		comment varchar(200) NOT NULL COMMENT '密钥注释',
+		created datetime NOT NULL COMMENT '创建时间',
+		updated datetime NOT NULL COMMENT '更新时间',
+		PRIMARY KEY (id),
+		UNIQUE KEY uniq_access_key (access_key),
+		KEY idx_uid (uid),
+		KEY idx_created (created),
+		KEY idx_updated (updated),
+		KEY idx_expire_at (expire_at)
+	  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='访问密钥表'`
+)
 
 // ModelUser 用户表
 type ModelUser struct {
-	ID        int       `json:"id,omitempty"`
-	UID       string    `json:"uid,omitempty"`
-	Password  string    `json:"password,omitempty"`
-	Email     string    `json:"email,omitempty"`
-	Mobile    string    `json:"mobile,omitempty"`
-	Extra     string    `json:"extra,omitempty"`
-	IsDeleted bool      `json:"is_deleted,omitempty"`
-	Created   time.Time `json:"created,omitempty"`
-	Updated   time.Time `json:"updated,omitempty"`
+	ID        int            `json:"id,omitempty"`
+	UID       string         `json:"uid,omitempty"`
+	Password  string         `json:"password,omitempty"`
+	Email     sql.NullString `json:"email,omitempty"`
+	Mobile    sql.NullString `json:"mobile,omitempty"`
+	Nickname  string         `json:"nickname,omitempty"`
+	Avatar    string         `json:"avatar,omitempty"`
+	Extra     string         `json:"extra,omitempty"`
+	LastLogin time.Time      `json:"last_login,omitempty"`
+	Created   time.Time      `json:"created,omitempty"`
+	Updated   time.Time      `json:"updated,omitempty"`
 }
 
 // ModelAuth 用户和第三方认证绑定表
@@ -22,17 +81,17 @@ type ModelAuth struct {
 	AuthName  string    `json:"auth_name,omitempty"`
 	AuthUID   string    `json:"auth_uid,omitempty"`
 	AuthExtra string    `json:"auth_extra,omitempty"`
-	IsDeleted bool      `json:"is_deleted,omitempty"`
 	Created   time.Time `json:"created,omitempty"`
 	Updated   time.Time `json:"updated,omitempty"`
 }
 
 // ModelAccessKey 访问密钥
 type ModelAccessKey struct {
-	ID      int       `json:"id,omitempty"`
-	Key     string    `json:"key,omitempty"`
-	UID     string    `json:"uid,omitempty"` // ModelUser UID
-	Comment string    `json:"comment,omitempty"`
-	Created time.Time `json:"created,omitempty"`
-	Updated time.Time `json:"updated,omitempty"`
+	ID        int       `json:"id,omitempty"`
+	AccessKey string    `json:"access_key,omitempty"`
+	UID       string    `json:"uid,omitempty"` // ModelUser UID
+	ExpireAt  time.Time `json:"expire_at,omitempty"`
+	Comment   string    `json:"comment,omitempty"`
+	Created   time.Time `json:"created,omitempty"`
+	Updated   time.Time `json:"updated,omitempty"`
 }
